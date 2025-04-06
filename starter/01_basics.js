e54ebe47e328c69715ae37bd9f5829cf0382041f
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2025-04-07 10:12:20 +1200

    gfeh

--- starter/01_basics.js
+++ starter/01_basics.js
@@ -2,8 +2,23 @@
 // TODO: Write a function 'printStudentName' that logs the student's name
 
 function printStudentName() {
-  // Your code here
-  console.log("hello")
+	// Your code here
+	class Student {
+		constructor(name, age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		printName() {
+			console.log("Student name is:", this.name);
+		}
+	}
+
+	const student = new Student("Ava", 20);
+
+	student.printName(); // Call the method to print the name
 }
+
+printStudentName(); // Call the function to test it
 
 module.exports = { printStudentName };
--- starter/02_parameters.js
+++ starter/02_parameters.js
@@ -1,8 +1,15 @@
 // TODO: Write a function 'totalScores' that takes an array of numbers
 // TODO: Return the sum of all numbers in the array
 
+totalScores([1, 2, 3, 4, 5]); // 15
+totalScores([10, 20, 30]); // 60
+
+
 function totalScores(scores) {
-  // Your code here
+	// Your code here
+	totalScore = scores.reduce((current, total) => current + total, 0);
+	console.log(totalScore); // Print the total score
+	return totalScores;
 }
 
 module.exports = { totalScores };
